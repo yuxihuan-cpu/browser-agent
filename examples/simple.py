cfb3dbdb9861e9e1dc68f5dcf623d8cb3afdfef3
Magnus Müller <67061560+MagMueller@users.noreply.github.com>
2025-10-23 23:22:32 -0700

    fix link docs

--- examples/simple.py
+++ examples/simple.py
@@ -11,7 +11,8 @@ from browser_use import Agent, ChatBrowserUse
 load_dotenv()
 
 agent = Agent(
-	task='Find the number of stars of the following repos: browser-use, playwright, stagehand, react, nextjs',
+	task='https://wcpdev.wd101.myworkday.com/wday/authgwy/nayyabow_wcpdev2/login.html?returnTo=%2fnayyabow_wcpdev2%2fd%2fhome.html go here click and then fill with example data, use only send keys ',
+	# task='go to duckduckgo.com search for browser-use but use only send keys validate the results, go to first link',
 	llm=ChatBrowserUse(),
 )
 agent.run_sync()
